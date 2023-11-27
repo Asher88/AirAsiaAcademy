9186be1cbadf9f99cba9955cc593c2727d4e0bf2
Asher88 <aku.ashaari@gmail.com>
2023-11-27 12:07:55 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -35,8 +35,8 @@ elif option=='T n C':
 else:
     st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
     show = st.checkbox('I agree the terms and conditions')
-if show:
-    st.write(pd.DataFrame({
-    'Intplan': ['yes', 'yes', 'yes', 'no'],
-    'Churn Status': [0, 0, 0, 1]
-    }))
+    if show:
+        st.write(pd.DataFrame({
+        'Intplan': ['yes', 'yes', 'yes', 'no'],
+        'Churn Status': [0, 0, 0, 1]
+        }))
